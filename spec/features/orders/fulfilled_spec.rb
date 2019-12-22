a0978ea2bdbde58afc80647c354b3de8fc3a6d7f
Santiago Caraveo <santiagocbrvg@hotmail.com>
2019-12-21 23:50:48 -0700

    adds test for the pagege method on teh order controller

---
+++ spec/features/orders/fulfilled_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'when all items have been fulfilled in an order'do
+  xit "can change status of an order to packaged" do
+
+    mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+    tire = mike.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+    paper = mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
+
+    visit "/items/#{paper.id}"
+    click_on "Add To Cart"
+    visit "/items/#{tire.id}"
+    click_on "Add To Cart"
+
+    visit '/cart'
+
+    click_on "Checkout"
+
+    name = "Bert"
+    address = "123 Sesame St."
+    city = "NYC"
+    state = "New York"
+    zip = 10001
+
+    fill_in :name, with: name
+    fill_in :address, with: address
+    fill_in :city, with: city
+    fill_in :state, with: state
+    fill_in :zip, with: zip
+
+    click_button "Create Order"
+
+
+  end
+end
