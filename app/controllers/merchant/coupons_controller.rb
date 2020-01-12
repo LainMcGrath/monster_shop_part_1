9e844b7f84a5ce77013724cc5c1341cd98014701
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-11 22:14:55 -0700

    Add delete method

--- app/controllers/merchant/coupons_controller.rb
+++ app/controllers/merchant/coupons_controller.rb
@@ -11,13 +11,42 @@ class Merchant::CouponsController < Merchant::BaseController
     if @coupon.save
       redirect_to "/merchant"
       flash[:notice] = "Coupon was successfully created"
+    else
+      flash[:notice] = @coupon.errors.full_messages.to_sentence
+      render :new
     end
+  end
+
+  def edit
+    merchant = Merchant.find_by(params[id: :merchant_id])
+    @coupon = merchant.coupons.find_by(params[id: :coupon_id])
+  end
+
+  def update
+    @coupon = Coupon.find_by(params[id: :coupon_id])
+    @coupon.update(coupon_params)
+    if @coupon.save
+      redirect_to "/merchant"
+      flash[:notice] = "Coupon was updated"
+    end
+  end
+
+  def show
+    merchant = Merchant.find_by(params[id: :merchant_id])
+    @coupon = merchant.coupons.find_by(params[id: :coupon_id])
+  end
+
+  def destroy
+    coupon = Coupon.find_by(params[id: :coupon_id])
+    coupon.destroy
+    redirect_to "/merchant"
+    flash[:notice] = "#{coupon.name} has been deleted"
   end
 
  private
 
   def coupon_params
-    params.require(:coupon).permit(:name, :code, :discount)
+    params.require(:coupon).permit(:name,:code,:discount)
   end
 
 end
