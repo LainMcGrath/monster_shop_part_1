5bd3361ce833dac2e4ba0d396cd861800e020151
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-11 19:25:54 -0700

    add create new coupon test

---
+++ spec/features/coupons/new_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "Create coupon" do
+  describe "New coupon" do
+    before :each do
+      @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 80203)
+      @merchant_admin = @bike_shop.users.create!(name: "Merchant Admin", address: "1230 East Street", city: "Boulder", state: "CO", zip: 98273, email: "merchant_admin@merchant_admin.com", password: "merchant_admin", password_confirmation: "merchant_admin", role: 2)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant_admin)
+    end
+
+    it 'A merchant can create a new coupon' do
+      visit "/merchant"
+
+      click_on("Create new coupon")
+      expect(current_path).to eq("/merchant/coupons/new")
+
+      name = "25% off all items!"
+      code = "WOOF"
+      discount = 25.0
+
+      fill_in 'Name', with: name
+      fill_in 'Code', with: code
+      fill_in 'Discount', with: discount
+
+      click_on "Create Coupon"
+
+      new_coupon = Coupon.last
+
+      expect(current_path).to eq("/merchant")
+      expect(page).to have_content("Coupon was successfully created")
+
+      within "#coupons" do
+        within "#coupon-#{new_coupon.id}" do
+          expect(page).to have_content("Name: #{new_coupon.name}")
+          expect(page).to have_content("Code: #{new_coupon.code}")
+          expect(page).to have_content("Discount: 25%")
+        end
+      end
+    end
+  end
+end
