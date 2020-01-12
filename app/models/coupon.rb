56636159b382ab835dd746a293319798e69ee8a4
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-11 23:41:47 -0700

    Add unique code validation

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -4,6 +4,7 @@ class Coupon < ApplicationRecord
 
   validates_presence_of :name
   validates_presence_of :code
+  validates :code, uniqueness: true, presence: true
   validates_presence_of :discount
   # validates :discount, numericality: { less_than_or_equal_to: 100, only_integer: true }
   validates :discount, numericality: { less_than_or_equal_to: 100 }
