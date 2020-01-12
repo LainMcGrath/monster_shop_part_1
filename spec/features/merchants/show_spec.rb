6d37110395268a3e562a181f9270db7f5735b536
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-11 23:34:08 -0700

    Add coupon tests

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -23,5 +23,24 @@ RSpec.describe 'merchant show page', type: :feature do
       expect(current_path).to eq("/merchants/#{@bike_shop.id}/items")
     end
 
+    xit "a user can see all coupons for that merchant" do
+      visit "/merchants/#{@bike_shop.id}"
+
+      coupon_1 = Coupon.create(name: "15% off", code: "15%", discount: 15, merchant_id: @bike_shop.id)
+      coupon_2 = Coupon.create(name: "20% off", code: "20%", discount: 20, merchant_id: @bike_shop.id)
+      coupon_3 = Coupon.create(name: "25% off", code: "25%", discount: 25, merchant_id: @bike_shop.id)
+
+      within "#coupon-#{coupon_1.id}" do
+        expect(page).to have_content("Name: #{coupon_1.name}")
+        expect(page).to have_content("Code: #{coupon_1.code}")
+        expect(page).to have_content("Discount: 15%")
+      end
+    end
+
+    it "a user sees a notification if the merchant does not have any coupons" do
+      visit "/merchants/#{@bike_shop.id}"
+
+      expect(page).to have_content("This merchant does currently have any coupons")
+    end
   end
 end
