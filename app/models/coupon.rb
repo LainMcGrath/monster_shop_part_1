3ea9da949444f7084be74400ba69ed629944502b
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-12 21:01:48 -0700

    Add name uniqueness validation

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -3,10 +3,10 @@ class Coupon < ApplicationRecord
   has_many :orders
 
   validates_presence_of :name
+  validates :name, uniqueness: true, presence: true
   validates_presence_of :code
   validates :code, uniqueness: true, presence: true
   validates_presence_of :discount
   # validates :discount, numericality: { less_than_or_equal_to: 100, only_integer: true }
   validates :discount, numericality: { less_than_or_equal_to: 100 }
-
 end
