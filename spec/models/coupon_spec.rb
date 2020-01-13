bc9813245dce872d39d903df90b4c6bbbd5ed118
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-12 21:06:03 -0700

    Add name uniqueness test

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -8,11 +8,20 @@ RSpec.describe Coupon do
 
   describe "validations" do
     it { should validate_presence_of :name }
+    it { should validate_uniqueness_of :name}
     it { should validate_presence_of :code }
     it {should validate_uniqueness_of :code}
     it { should validate_presence_of :discount }
     it { validate_numericality_of(:discount).is_less_than(100) }
     # it { validate_numericality_of(:discount).only_integer }
-
   end
+  # 
+  # describe "instance methods" do
+  #   before(:each) do
+  #     @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+  #     @tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+  #     @coupon_1 = Coupon.create(name: "15% off", code: "WOOF!", discount: 15, merchant_id: meg.id)
+  # end
+
+
 end
