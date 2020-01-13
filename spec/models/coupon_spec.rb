2f129f2d6f8b4f6c2f4e9775f841b0725a7d7094
Lain McGratH <lain.mcgrath@gusto.com>
2020-01-13 14:06:03 -0700

    Remove commented out integer validation

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -13,9 +13,8 @@ RSpec.describe Coupon do
     it {should validate_uniqueness_of :code}
     it { should validate_presence_of :discount }
     it { validate_numericality_of(:discount).is_less_than(100) }
-    # it { validate_numericality_of(:discount).only_integer }
   end
-  # 
+  #
   # describe "instance methods" do
   #   before(:each) do
   #     @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
